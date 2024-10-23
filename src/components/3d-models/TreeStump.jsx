e2285212318af284c58ea002263088528dbdd30f
jcv-dev <valenciarivasjuancamilo@gmail.com>
2024-10-23 09:50:20 -0500

    Add tree models

--- src/components/3d-models/TreeStump.jsx
+++ src/components/3d-models/TreeStump.jsx
@@ -1,56 +1,43 @@
-import { useGLTF } from "@react-three/drei";
-import { useMemo } from "react";
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
 
-export function TreeStump(props) {
-  const {nodes, materials} = useGLTF("/models/tree_stump.glb");
- 
+export default function TreeStump(props) {
+  const { nodes, materials } = useGLTF('/models/tree-stump.glb')
   return (
     <group {...props} dispose={null}>
       <group name="Sketchfab_Scene">
-        <group
-          name="Sketchfab_model"
-          rotation={[-Math.PI / 2, 0, 0]}
-          scale={0.453}
-        >
-          <group
-            name="49ec9fea3af9403fa3a6e433f3e91e2dfbx"
-            rotation={[Math.PI / 2, 0, 0]}
-            scale={0.01}
-          >
-            <group name="RootNode">
-              <group
-                name="Cylinder"
-                rotation={[-Math.PI / 2, 0, 0]}
-                scale={[100, 100, 28.479]}
-              >
-                <mesh
-                  name="Cylinder_Material002_0"
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Cylinder_Material002_0.geometry}
-                  material={materials["Material.002"]}
-                />
-              </group>
-              <group
-                name="Cylinder001"
-                position={[-14.86, 52.407, -105.789]}
-                rotation={[-2.577, 0, Math.PI]}
-                scale={[-10.392, 11.571, 31.282]}
-              >
-                <mesh
-                  name="Cylinder001_Material003_0"
-                  castShadow
-                  receiveShadow
-                  geometry={nodes.Cylinder001_Material003_0.geometry}
-                  material={materials["Material.003"]}
-                />
+        <group name="Sketchfab_model" rotation={[-Math.PI / 2, 0, -Math.PI]}>
+          <group name="root">
+            <group name="GLTF_SceneRootNode" rotation={[Math.PI / 2, 0, 0]}>
+              <group name="_2">
+                <group name="circle_0" position={[0.034, 0.279, -0.099]} rotation={[0, -0.087, 0]}>
+                  <mesh
+                    name="Object_5"
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_5.geometry}
+                    material={materials.material_0}
+                  />
+                </group>
+                <group
+                  name="cylinder_1"
+                  position={[-0.438, 0.236, -0.563]}
+                  rotation={[-0.283, 1.254, 0.182]}>
+                  <mesh
+                    name="Object_7"
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_7.geometry}
+                    material={materials.material_1}
+                  />
+                </group>
               </group>
             </group>
           </group>
         </group>
       </group>
     </group>
-  );
+  )
 }
 
-useGLTF.preload("/tree_stump.glb");
+useGLTF.preload('/models/tree-stump.glb')
